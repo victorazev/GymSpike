bd6634f5799126a51c9569548bbd9753540abb7a
tiago.gandra <tiagogandrati@gmail.com>
2024-12-11 15:55:35 -0300

    feat: servidor express

--- back-end/src/server.ts
+++ back-end/src/server.ts
@@ -1,1 +1,7 @@
-console.log("Hello world!");
+import app from './app';
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Servidor rodando na porta ${PORT}`);
+});
