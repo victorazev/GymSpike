bceba5acb6002cbd46861e3245bc7ea82f7c02df
Tiago.Gandra <tiagogandrati@gmail.com>
2024-12-11 22:53:01 -0300

    feat: conexao com database cloud

--- back-end/src/config/database.ts
+++ back-end/src/config/database.ts
@@ -1,67 +1,25 @@
-// // Conexão com banco de dados
-// import mongoose from 'mongoose';
-// import dotenv from 'dotenv';
-
-// dotenv.config();
-
-// export const connectionDataBase = async () => {
-// 	const mongoURI = process.env.DATA_BASE_CONNECTION || '';
-// 	console.log('env do mongo', mongoURI);
-// 	try {
-// 		await mongoose.connect(mongoURI);
-// 		console.log('MongoDB connection successfully');
-// 	} catch (error) {
-// 		console.error('Deu ruim:', error);
-// 	}
-// };
-
-// const { MongoClient, ServerApiVersion } = require('mongodb');
-// const uri =
-// 	'mongodb+srv://victorazevdev:LBOQhipuHGVMNaBd@cluster0.mnbdz.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
-// console.log(uri);
-// // Create a MongoClient with a MongoClientOptions object to set the Stable API version
-// const client = new MongoClient(uri, {
-// 	serverApi: {
-// 		version: ServerApiVersion.v1,
-// 		strict: true,
-// 		deprecationErrors: true,
-// 	},
-// });
-// async function run() {
-// 	try {
-// 		// Connect the client to the server    (optional starting in v4.7)
-// 		await client.connect();
-// 		// Send a ping to confirm a successful connection
-// 		await client.db('admin').command({ ping: 1 });
-// 		console.log(
-// 			'Pinged your deployment. You successfully connected to MongoDB!',
-// 		);
-// 	} finally {
-// 		// Ensures that the client will close when you finish/error
-// 		await client.close();
-// 	}
-// }
-// run().catch(console.dir);
 
 import mongoose from 'mongoose';
 
 type TInput = {
-	db: string;
-};
-export default ({ db }: TInput) => {
-	const connect = () => {
-		mongoose
-			.connect(db)
-			.set('useNewUrlParser', true)
-			.then(() => {
-				return console.info(`Successfully connected to ${db}`);
-			})
-			.catch((error) => {
-				console.error('Error connecting to database: ', error);
-				return process.exit(1);
-			});
-	};
-	connect();
+  db: string;
+}
+export default ({db}: TInput) => {
+  
+  const connect = () => {
+    mongoose
+      .connect(
+        db
+      )
+      .then(() => {
+        return console.info(`Successfully connected to ${db}`);
+      })
+      .catch(error => {
+        console.error('Error connecting to database: ', error);
+        return process.exit(1);
+      });
+  };
+  connect();
 
-	mongoose.connection.on('disconnected', connect);
+  mongoose.connection.on('disconnected', connect);
 };
--- back-end/src/server.ts
+++ back-end/src/server.ts
@@ -1,10 +1,13 @@
 import app from './app';
 import connect from './config/database';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const PORT = process.env.PORT || 3000;
 
 async function startServer() {
-	// await database.connectionDataBase();
+	const db = process.env.STRING_CONNECTION || '';
 
 	connect({ db });
 
