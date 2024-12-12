53c32095f580b4d78ca65e7d21d22b13bd40d846
Victor Augusto <augustovictor85@gmail.com>
2024-12-11 22:33:44 -0300

    config(banco de dados): teste

--- back-end/src/server.ts
+++ back-end/src/server.ts
@@ -1,7 +1,16 @@
 import app from './app';
+import connect from './config/database';
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
-  console.log(`Servidor rodando na porta ${PORT}`);
-});
+async function startServer() {
+	// await database.connectionDataBase();
+
+	connect({ db });
+
+	app.listen(PORT, () => {
+		console.log(`Servidor rodando na porta ${PORT}`);
+	});
+}
+
+startServer();
