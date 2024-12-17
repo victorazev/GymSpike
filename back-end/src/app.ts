fc59cf535e2d68e36b66d9f4a3996eae3b2c87a8
Victor Augusto <augustovictor85@gmail.com>
2024-12-17 00:32:37 -0300

    feat(activityGoals): adiciona rota, controlador e serviço

--- back-end/src/app.ts
+++ back-end/src/app.ts
@@ -1,6 +1,7 @@
 import express from 'express'; // Importa o Express, que é o framework usado para criar o servidor
 import userRoutes from './routes/user.routes'; // Importa as rotas de usuário definidas em outro arquivo
 import activityRoutes from './routes/activity.routes';
+import activityGoalsRoutes from './routes/activityGoals.routes';
 
 const app = express(); // Cria uma instância da aplicação Express
 
@@ -9,12 +10,13 @@ app.use(express.json());
 
 // Rota de teste (GET '/'): Verifica se o servidor está funcionando
 app.get('/', (req, res) => {
-  res.send('Servidor está funcionando!'); // Resposta simples para confirmar que o servidor está funcionando
+	res.send('Servidor está funcionando!'); // Resposta simples para confirmar que o servidor está funcionando
 });
 
 // Usa as rotas de usuários a partir do caminho '/api/users'
 app.use('/api/users', userRoutes);
 app.use('/api/users', activityRoutes);
+app.use('/api/users', activityGoalsRoutes);
 
 // Exporta a aplicação para ser usada em outro arquivo (por exemplo, para iniciar o servidor)
 export default app;
