1bc69f5e63f447a131e5c126e233d57aaba6416c
Victor Augusto <augustovictor85@gmail.com>
2024-12-22 00:55:59 -0300

    feat(app layout): adiciona layout padrão

--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -1,17 +1,32 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Login from "./pages/Login/Login";
-import { Cadastro, Cadastro2 } from "./pages/Cadastro/Cadastro";
+import {
+	BrowserRouter as Router,
+	Routes,
+	Route,
+	Navigate,
+} from 'react-router-dom';
+
+import Login from './pages/Login/Login';
+import { Cadastro, Cadastro2 } from './pages/Cadastro/Cadastro';
+import AppLayout from './pages/AppLayout/AppLayout';
+import Dashboard from './pages/Dashboard/Dashboard';
 
 function App() {
-  return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Login />} />
-        <Route path="/cadastro" element={<Cadastro />} />
-        <Route path="/cadastro2" element={<Cadastro2 />} />
-      </Routes>
-    </Router>
-  );
+	return (
+		<Router>
+			<Routes>
+				<Route element={<AppLayout />}>
+					<Route
+						index
+						element={<Navigate replace to="dashboard" />}
+					/>
+					<Route path="dashboard" element={<Dashboard />} />
+				</Route>
+				<Route path="login" element={<Login />} />
+				<Route path="cadastro" element={<Cadastro />} />
+				<Route path="cadastro2" element={<Cadastro2 />} />
+			</Routes>
+		</Router>
+	);
 }
 
 export default App;
