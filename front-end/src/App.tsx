843bbaa46c60478a96c1808f858719246d3f3086
Victor Augusto <augustovictor85@gmail.com>
2024-12-23 22:17:16 -0300

    feat(userActivity): adiciona página de atividades do usuário

--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -9,6 +9,7 @@ import Login from './pages/Login/Login';
 import { Cadastro, Cadastro2 } from './pages/Cadastro/Cadastro';
 import AppLayout from './pages/AppLayout/AppLayout';
 import Dashboard from './pages/Dashboard/Dashboard';
+import UserActivity from './pages/UserActivity/UserActivity';
 
 function App() {
 	return (
@@ -20,6 +21,10 @@ function App() {
 						element={<Navigate replace to="dashboard" />}
 					/>
 					<Route path="dashboard" element={<Dashboard />} />
+					<Route
+						path="userActivity"
+						element={<UserActivity />}
+					/>
 				</Route>
 				<Route path="login" element={<Login />} />
 				<Route path="cadastro" element={<Cadastro />} />
