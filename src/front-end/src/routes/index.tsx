257131780dcefc75f45b841a7eb655bd85aefd56
tiago.gandra <tiagogandrati@gmail.com>
2025-01-09 22:10:38 -0300

    fix: varios fixes

--- src/front-end/src/routes/index.tsx
+++ src/front-end/src/routes/index.tsx
@@ -4,7 +4,6 @@ import AppRoutes from "./app.routes";
 import { BrowserRouter as Router, Routes, Route} from "react-router-dom";
 import Login from "../pages/Login/Login";
 import { Cadastro } from "../pages/Cadastro/Cadastro";
-import { User, Userconfig } from "../pages/User/User";
 
 const MainRoutes: React.FC = () => {
   const { isAuthenticated } = useAuth();
