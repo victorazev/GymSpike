71759cfdbe8c5e7f7a9becd99ab49b22f6d1b002
Victor Augusto <augustovictor85@gmail.com>
2025-01-17 23:37:49 -0300

    feat(Index): add Toaster event

--- src/front-end/src/routes/index.tsx
+++ src/front-end/src/routes/index.tsx
@@ -1,29 +1,57 @@
-import React from "react";
-import { useAuth } from "../contexts/auth";
-import AppRoutes from "./app.routes";
-import { BrowserRouter as Router, Routes, Route} from "react-router-dom";
-import Login from "../pages/Login/Login";
-import { Cadastro } from "../pages/Cadastro/Cadastro";
+import React from 'react';
+import {
+	BrowserRouter as Router,
+	Routes,
+	Route,
+} from 'react-router-dom';
+import { Toaster } from 'react-hot-toast';
+
+import AppRoutes from './app.routes';
+import { useAuth } from '../contexts/auth';
+
+import Login from '../pages/Login/Login';
+import { Cadastro } from '../pages/Cadastro/Cadastro';
 
 const MainRoutes: React.FC = () => {
-  const { isAuthenticated } = useAuth();
+	const { isAuthenticated } = useAuth();
 
-  return (
-    <Router>
-      <Routes>
-        {isAuthenticated ? (
-          <Route path="/*" element={<AppRoutes />} />
-        ) : (
-          <>
-            {/* Redirecionando para a página de login */}
-            <Route path="/login" element={<Login />} />
-            <Route path="/cadastro" element={<Cadastro />} />
-            <Route path="/*" element={<Login />}/>
-          </>
-        )}
-      </Routes>
-    </Router>
-  );
+	return (
+		<Router>
+			<Routes>
+				{isAuthenticated ? (
+					<Route path="/*" element={<AppRoutes />} />
+				) : (
+					<>
+						{/* Redirecionando para a página de login */}
+						<Route path="/login" element={<Login />} />
+						<Route path="/cadastro" element={<Cadastro />} />
+						<Route path="/*" element={<Login />} />
+					</>
+				)}
+			</Routes>
+
+			<Toaster
+				position="top-center"
+				gutter={12}
+				containerStyle={{ margin: '8px' }}
+				toastOptions={{
+					success: {
+						duration: 5000,
+					},
+					error: {
+						duration: 9000,
+					},
+					style: {
+						fontSize: '16px',
+						maxWidth: '500px',
+						padding: '16px 24px',
+						backgroundColor: '#0f0f0f',
+						color: '#bbbbbb',
+					},
+				}}
+			/>
+		</Router>
+	);
 };
 
 export default MainRoutes;
